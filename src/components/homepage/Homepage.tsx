607cc2ad0d383fa0af8e1dfda76e2978491919cb
Steven Zhu <stevenzhu1019@gmail.com>
2025-11-20 10:27:51 -0500

    Added tailwind

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -1,9 +1,16 @@
 import NavBar from '../containers/NavBar/NavBar'
+import PageCard from './PageCard'
 
 export default function Homepage(){
     return (
     <>
         <NavBar></NavBar>
+        <img src="null">{/*Introductory picture*/}</img>
+        <div className="">{/*Description*/}</div>
+        <section className="flex">
+            <PageCard></PageCard>
+            <PageCard></PageCard>
+        </section>
     </>
     )
 }
---
+++ src/components/homepage/PageCard.tsx
@@ -0,0 +1,7 @@
+export default function PageCard(){
+    return (
+    <>
+        <div className="flex-1">Huh</div>
+    </>
+    )
+}

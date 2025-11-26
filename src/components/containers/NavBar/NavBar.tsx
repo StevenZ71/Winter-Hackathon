f0443210d32694d0b9be0422c9f38006554138cb
Steven Zhu <stevenzhu1019@gmail.com>
2025-11-26 11:43:45 -0500

    Updated a little bit of css

--- src/components/containers/NavBar/NavBar.tsx
+++ src/components/containers/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 export default function NavBar(){
     return (
         <>
-            <div className='flex absolute top-0 w-lvw bg-orange-600'>
+            <div className='flex absolute top-0 w-screen bg-orange-600'>
                 <div className='flex-1 text-center m-auto inset-0'>
                     <a href='/' className='text-sky-500 hover:text-black'>Homepage</a>
                 </div>
--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -3,7 +3,7 @@ import NavBar from '../containers/NavBar/NavBar'
 export default function Homepage(){
     return (
     <>
-        <div className='h-screen w-lvw bg-[url(../../../vite.svg)] bg-contain bg-no-repeat bg-center'>{/*Background/Introductory picture*/}
+        <div className='h-screen w-screen bg-[url(../../../vite.svg)] bg-contain bg-no-repeat bg-center'>{/*Background/Introductory picture*/}
             <div className='flex flex-col justify-center h-screen'>
                 <div className='m-[2vh]'>
                     <a href='/battle'>Battle</a>

57197dc3800c9d7eb5e21b088dc81a8ec003bd32
Steven Zhu <stevenzhu1019@gmail.com>
2025-11-26 11:27:01 -0500

    Vertical Center fix

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -4,14 +4,14 @@ export default function Homepage(){
     return (
     <>
         <div className='h-screen w-lvw bg-[url(../../../vite.svg)] bg-contain bg-no-repeat bg-center'>{/*Background/Introductory picture*/}
-            <div className='flex flex-col relative top-[50vh] bottom-[50vh]'>
-                <div className='flex-1 m-[1vh]'>
+            <div className='flex flex-col justify-center h-screen'>
+                <div className='m-[1vh]'>
                     <a href='/battle' className='flex-1 m-[1vh]'>Battle</a>
                 </div>
-                <div className='flex-1 m-[1vh]'>
+                <div className='m-[1vh]'>
                     <a href='/collection' className='flex-1 m-[1vh]'>Collection</a>
                 </div>
-                <div className='flex-1 m-[1vh]'>
+                <div className='m-[1vh]'>
                     <a href='/instructions' className='flex-1 m-[1vh]'>How to Play</a>
                 </div>
             </div>

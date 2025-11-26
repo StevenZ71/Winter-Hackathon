86a3c473ce45e9a46f50c4d5fa1ea9a440adae7d
Ivan <catalystblow@gmail.com>
2025-11-26 12:11:27 -0500

    Started Cards

---
+++ src/components/collection/Card.tsx
@@ -0,0 +1,16 @@
+//cards is scaleable (not definite size, vh vw)
+//vh
+//vwh 
+//em for text
+
+
+export default function Card(props){
+    return (
+    <>
+        <div className="bg-amber-300">
+            {props.cardName}
+        </div>
+    </>
+    )
+}
+
--- src/components/collection/Collection.tsx
+++ src/components/collection/Collection.tsx
@@ -1,10 +1,12 @@
 import NavBar from '../containers/NavBar/NavBar'
+import Card from './Card'
 
 export default function Collection(){
     //two sections, card collection with deck creation, character selection and
     return (
     <>
         <NavBar></NavBar>
+        <Card cardName="ABC"/>
     </>
     )
 }

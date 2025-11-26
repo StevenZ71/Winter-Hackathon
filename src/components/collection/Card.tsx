fe304a387a3b8bcbffe0a289b15106ca42193681
Ivan <catalystblow@gmail.com>
2025-11-26 12:57:26 -0500

    Created a shape of a card

--- src/components/collection/Card.tsx
+++ src/components/collection/Card.tsx
@@ -2,13 +2,29 @@
 //vh
 //vwh 
 //em for text
+//make the shape of the card
 
+//created a div element
+const myBox: HTMLDivElement = document.createElement("div");
+
+myBox.style.width = "100px";
+myBox.style.height = "100px";
+myBox.style.border = "1px solid black";
+myBox.style.backgroundColor = "lightblue";
+
+//add text content
+myBox.textContent = "Card";
+
+//append to document body
+document.body.appendChild(myBox);
 
 export default function Card(props){
     return (
     <>
-        <div className="bg-amber-300">
-            {props.cardName}
+        <div className="bg-amber-300 min-h-[10vh] min-w-[15vw] p-4 shadow-lg flex items-center justify-center">
+            <span className="text-[1.5em] font-bold text-center">
+                {props.cardName}
+            </span>
         </div>
     </>
     )
---
+++ src/components/collection/CardPage.tsx
@@ -0,0 +1,12 @@
+import NavBar from '../containers/NavBar/NavBar'
+import Card from './Card'
+
+export default function CardPage(){
+    //card page that shows all the existing cards in one page
+    return (
+    <>
+        <NavBar></NavBar>
+        
+    </>
+    )
+}

4a9f77a714b31a300b09c06e1581ec597221dbb4
Steven Zhu <stevenzhu1019@gmail.com>
2025-12-10 11:25:19 -0500

    Button

--- react_files/src/components/collection/Collection.tsx
+++ react_files/src/components/collection/Collection.tsx
@@ -7,9 +7,13 @@ import CharacterPage from './CharacterPage'
 export default function Collection(){
     const [cardPage, setCardPage] = useState(true);
     //two sections, card collection with deck creation, character selection and 
+    const click = () =>{
+        setCardPage(!cardPage);
+    }
     return (
     <>
         <NavBar></NavBar>
+        <button onClick={click}>{cardPage ? 'Character Select' : 'Card Select'}</button>
         {cardPage ? <CardPage></CardPage> :
         <><CharacterPage> </CharacterPage>
         <CharacterCarousel></CharacterCarousel></>
